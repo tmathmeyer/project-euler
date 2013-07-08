466bf2477df2974ef575c9b69d1fb431ac183323
Ted Meyer <tmathmeyer@gmail.com>
2013-07-08 17:41:15 -0400

    problem 282. done mostly on paper and wolfram alpha, this is the result for each A(n,n)

--- src/problems/lib/java/Sieve.java
+++ src/problems/lib/java/Sieve.java
@@ -30,44 +30,6 @@ public class Sieve
 		}
 		return res;
 	}
-	
-	public static int[] largeSieve(int top)
-	{
-		boolean[] vals = new boolean[top+1];
-		int size = 0;
-		vals[0] = false;
-		vals[1] = false;
-		
-		for(int i=2; i <= top; i++)
-		{
-			if (!vals[i])
-			{
-				for(int j = i*2; j<=top; j+=i)
-				{
-					vals[j] = true;
-				}
-				size++;
-			}
-		}
-
-		vals = null;
-		int pos = 0;
-		int[] primes = new int[size];
-		for(int i = 1; i <= top; i++)
-		{
-			if (Numbers.isPrime(i))
-			{
-				primes[pos] = i;
-				pos++;
-			}
-			if (i % 1000000 == 0){
-				System.out.println(i/1000000);
-			}
-		}
-		
-		return primes;
-		
-	}
 }
 
 class IL 
